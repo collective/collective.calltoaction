18c7aa99c586ed265c7392dbec4ef8beee711abe
Maurits van Rees <maurits@vanrees.org>
2016-03-31 21:12:34 +0200

    Use an actual overlay.
    Use jquerytools for this.
    Added a bit of css.
    See for example http://jquerytools.github.io/demos/overlay/trigger.html

--- src/collective/calltoaction/browser/static/calltoaction.js
+++ src/collective/calltoaction/browser/static/calltoaction.js
@@ -4,8 +4,28 @@
     $('.calltoaction-portlet-wrapper').each( function() {
       var timeout = $(this).attr('data-timeout');
       var el = $(this);
-      el.hide();
-      setTimeout(function(){ el.show();}, timeout);
+      setTimeout(
+        function(){
+          // Overlay adapted from http://jquerytools.github.io/demos/overlay/trigger.html
+          el.overlay({
+            // custom top position
+            top: 260,
+            // some mask tweaks suitable for facebox-looking dialogs
+            mask: {
+              // you might also consider a "transparent" color for the mask
+              color: '#fff',
+              // load mask a little faster
+              loadSpeed: 200,
+              // very transparent
+              opacity: 0.5
+            },
+            // disable this for modal dialog-type of overlays
+            closeOnClick: true,
+            // load it immediately after the construction
+            load: true
+          });
+        },
+        timeout);
     });
   });
 })(jQuery);
