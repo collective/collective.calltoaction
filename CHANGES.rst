c4f704d2d4d4c267b3d38a7f4afd35bd3ac55630
Jean-Paul Ladage <j.ladage@zestsoftware.nl>
2016-04-20 12:07:12 +0200

    Back to development: 1.0rc2

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+1.0rc2 (unreleased)
+-------------------
+
+- Nothing changed yet.
+
+
 1.0rc1 (2016-04-20)
 -------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.calltoaction',
-    version='1.0rc1',
+    version='1.0rc2.dev0',
     description="Portlet with call to action",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
